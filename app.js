fcb6316abef47cbbb06eafb0e712f4f1380c4a11
Rawle Becerril <rawlesxm@gmail.com>
2022-10-22 16:35:08 -0400

    testing nested ajax calls

--- app.js
+++ app.js
@@ -10,30 +10,42 @@ function searchCharacter (character1, character2, character3){
     url3 = `${baseUrl}character/?name=${character3}`
     console.log(url3)
 
+    let char1Episodes = []
+    let char2Episodes = []
+    let char3Episodes = []
+    let $divFirstEpisodes = $(".firstEpisodes")
+
     $.ajax(url1)
     .then ((info1) => {
         console.log(character1)
         console.log(info1.info.count + " total")
         console.log(info1.info.pages + " pages")
         console.log(info1.results[0].episode)
-
+        char1Episodes = info1.results[0].episode
+        $divFirstEpisodes.append(char1Episodes)
     })
 
-    $.ajax(url2)
-    .then ((info2) => {
-        console.log(character2)
-        console.log(info2.info.count + " total")
-        console.log(info2.info.pages + " pages")
-        console.log(info2.results[0].episode)
-    })    
+    // $.ajax(url2)
+    // .then ((info2) => {
+    //     console.log(character2)
+    //     console.log(info2.info.count + " total")
+    //     console.log(info2.info.pages + " pages")
+    //     console.log(info2.results[0].episode)
+    //     char2Episodes = info2.results[0].episode
+    //     return char2Episodes
+    // })    
 
-    $.ajax(url3)
-    .then ((info3) => {
-        console.log(character3)
-        console.log(info3.info.count + " total")
-        console.log(info3.info.pages + " pages")
-        console.log(info3.results[0].episode)
-    })
+    // $.ajax(url3)
+    // .then ((info3) => {
+    //     console.log(character3)
+    //     console.log(info3.info.count + " total")
+    //     console.log(info3.info.pages + " pages")
+    //     console.log(info3.results[0].episode)
+    //     char3Episodes = info3.results[0].episode
+    //     return char3Episodes        
+    // }) 
+    
+    console.log(char1Episodes)
 }
 
 searchCharacter("rick", "morty", "jerry")
