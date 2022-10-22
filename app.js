3ba7f081f6a36bf7c7df5757300060c68068f16a
Rawle Becerril <rawlesxm@gmail.com>
2022-10-22 14:08:09 -0400

    instituting search features

---
+++ app.js
@@ -0,0 +1,36 @@
+const baseUrl = "https://rickandmortyapi.com/api/"
+
+function searchCharacter (character1, character2, character3){
+    url1 = `${baseUrl}character/?name=${character1}`
+    console.log(url1)
+
+    url2 = `${baseUrl}character/?name=${character2}`
+    console.log(url2)
+
+    url3 = `${baseUrl}character/?name=${character3}`
+    console.log(url3)
+
+    $.ajax(url1)
+    .then ((info1) => {
+        console.log(info1.info)
+        console.log(info1.info.count)
+        console.log(info1.results)
+
+    })
+
+    $.ajax(url2)
+    .then ((info2) => {
+        console.log(info2.info)
+        console.log(info2.info.count)
+        console.log(info2.results)
+    })    
+
+    $.ajax(url3)
+    .then ((info3) => {
+        console.log(info3.info)
+        console.log(info3.info.count)
+        console.log(info3.results)
+    })
+}
+
+searchCharacter("rick", "morty", "jerry")
