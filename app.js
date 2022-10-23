b9b0abbb0bd4e9af6f9aee8d4320443b4ba0df47
Rawle Becerril <rawlesxm@gmail.com>
2022-10-23 00:28:59 -0400

    cleaned up

--- app.js
+++ app.js
@@ -32,10 +32,6 @@ function searchCharacter (character1, character2, character3){
                 char3Episodes=info3.results[0].episode
                 console.log(char3Episodes)
 
-                console.log(char1Episodes[0])
-                console.log(char2Episodes[0])
-                console.log(char3Episodes[0])
-
                 for (let i = 0; i < char1Episodes.length; i++){
                     for (let j = 0; j < char2Episodes.length; j++){
                         for (let k = 0; k < char3Episodes.length; k++){
@@ -48,30 +44,7 @@ function searchCharacter (character1, character2, character3){
             })
         })
         
-    })
-
-    
-   
-    // $.ajax(url2)
-    // .then ((info2) => {
-    //     console.log(character2)
-    //     console.log(info2.info.count + " total")
-    //     console.log(info2.info.pages + " pages")
-    //     console.log(info2.results[0].episode)
-    //     char2Episodes = info2.results[0].episode
-    //     return char2Episodes
-    // })    
-
-    // $.ajax(url3)
-    // .then ((info3) => {
-    //     console.log(character3)
-    //     console.log(info3.info.count + " total")
-    //     console.log(info3.info.pages + " pages")
-    //     console.log(info3.results[0].episode)
-    //     char3Episodes = info3.results[0].episode
-    //     return char3Episodes        
-    // }) 
-    
+    })    
     console.log(sharedEpisodes)
 }
 
