0d68f92a5a5f4105f9777794429f26a662e60764
Rawle Becerril <rawlesxm@gmail.com>
2022-10-24 19:31:04 -0400

    figured out array of shared # and name

--- app.js
+++ app.js
@@ -47,7 +47,9 @@ function searchCharacter (character1, character2, character3){
                 for (episode of sharedEpisodes){
                     $.ajax(episode)
                     .then((data) =>{
-                        console.log(`${data.name}, ${data.episode}`)
+                        console.log(`{${data.name}, ${data.episode}}`)
+                        episodeNameNumbers.push(`{${data.name}, ${data.episode}}`)
+                        console.log(episodeNameNumbers)
                     })
                 }
             }) 
